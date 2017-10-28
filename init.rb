ee6c225543f18438573da658fe1024d5a4baf16c
Daniel Kastl <daniel@georepublic.de>
2017-10-28 11:38:30 +0900

    Increases version to 1.1.0

--- init.rb
+++ init.rb
@@ -11,7 +11,7 @@ Redmine::Plugin.register :redmine_admissions do
   author 'Jens Krämer, Georepublic'
   author_url 'https://hub.georepublic.net/gtt/redmine_admissions'
   description 'Allows users to self-join selected Projects'
-  version '1.0.0'
+  version '1.1.0'
 
   requires_redmine version_or_higher: '3.4.0'
 
