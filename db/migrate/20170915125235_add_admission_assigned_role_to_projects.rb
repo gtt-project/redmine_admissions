d8286641d6675641b90aa2e020573a33b7fc2626
Thibault <933kss@gmail.com>
2019-01-25 13:37:50 +0900

    add rails version to migrations

--- db/migrate/20170915125235_add_admission_assigned_role_to_projects.rb
+++ db/migrate/20170915125235_add_admission_assigned_role_to_projects.rb
@@ -1,4 +1,4 @@
-class AddAdmissionAssignedRoleToProjects < ActiveRecord::Migration
+class AddAdmissionAssignedRoleToProjects < ActiveRecord::Migration[5.2]
   def change
     add_column :projects, :admission_assigned_role_id, :integer
     add_foreign_key :projects, :roles,
