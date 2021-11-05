49eba6f2517c6eed8f3e5c99281d44f0622a3e74
Daniel Kastl <daniel@georepublic.de>
2021-11-05 17:34:01 +0900

    Publishing on Github
    Signed-off-by: Daniel Kastl <daniel@georepublic.de>

--- init.rb
+++ init.rb
@@ -9,9 +9,10 @@ end
 Redmine::Plugin.register :redmine_admissions do
   name 'Redmine Admissions Plugin'
   author 'Jens Krämer, Georepublic'
-  author_url 'https://hub.georepublic.net/gtt/redmine_admissions'
+  author_url 'https://github.com/georepublic'
+  url 'https://github.com/gtt-project/redmine_admissions'
   description 'Allows users to self-join selected Projects'
-  version '1.1.0'
+  version '1.2.0'
 
   requires_redmine version_or_higher: '3.4.0'
 
