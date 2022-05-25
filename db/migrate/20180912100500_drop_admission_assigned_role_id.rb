315852dbd1a21b3926adb663fb4621e0bc91622e
Ko Nagase <nagase@georepublic.co.jp>
2022-05-26 07:03:45 +0900

    Fixed DropAdmissionAssignedRoleId migration for redmine 4.2 (rails 5.2)

--- db/migrate/20180912100500_drop_admission_assigned_role_id.rb
+++ db/migrate/20180912100500_drop_admission_assigned_role_id.rb
@@ -1,8 +1,6 @@
 class DropAdmissionAssignedRoleId < ActiveRecord::Migration[5.2]
   def change
-    remove_foreign_key :projects, :roles,
-      column: :admission_assigned_role_id,
-      on_delete: :nullify
+    remove_foreign_key :projects, column: :admission_assigned_role_id
     remove_column :projects, :admission_assigned_role_id, :integer
   end
 end
